f075eab288add040be37f3b1fc77e4ecca31f646
zoueinj <zwayn@proton.me>
2023-11-10 18:48:16 +0000

    generate:add script to run full pipeline
    encodes video using the right scenario, then calls aom inspect and finally run python to extract motion from json

---
+++ generate.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+input=$1
+output=$2
+width=$3
+height=$4
+fps=$5
+
+./src/enc_scenario/s3-scc-01.sh $input $output $width $height $fps
+./aom_build/examples/inspect output/ivf/$output.ivf -mv -r > output/json/$output.json
+python3.10 main.py --input $output
