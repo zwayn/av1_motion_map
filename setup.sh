68a62b57590d6553168ed65664b16994bf265374
zoueinj <zwayn@proton.me>
2023-11-10 18:49:20 +0000

    setup:modify aom build to add inspect

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+python3.10 -m pip install -r requirements.txt
+
 git clone https://aomedia.googlesource.com/aom
 
 mkdir aom_build
 cd aom_build
-cmake ../aom/
+cmake ../aom/ -DCONFIG_TUNE_VMAF=1 -DENABLE_CCACHE=1 -DCONFIG_INSPECTION=1
 make -j8
