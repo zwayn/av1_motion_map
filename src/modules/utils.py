6cc0d0be0e67e597d0c04d561e84516f07122d15
Julien Zouein <julien.zouein@gmail.com>
2023-12-19 10:31:03 +0100

    src/modules/utils: add csv writing functions

--- src/modules/utils.py
+++ src/modules/utils.py
@@ -12,6 +12,7 @@ Functions useful for the whole project.
 
 import os
 
+import csv
 import numpy as np
 
 
@@ -35,9 +36,39 @@ def get_paths(directory: str) -> list:
     return paths
 
 
+def init_csv(output_path: str, complexity_metrics: list, quality_metrics: list, motion_metrics: list) -> None:
+    """
+    Initialise the CSV file.
+    :param output_path: path of the output file
+    :param complexity_metrics: list of complexity metrics
+    :param quality_metrics: list of quality metrics
+    :param motion_metrics: list of motion metrics
+    :return: None
+    """
+    cols = ["frame_number"]
+    cols.extend(complexity_metrics)
+    cols.extend(quality_metrics)
+    cols.extend(motion_metrics)
+    with open(f"{output_path}log_metrics.csv", 'a') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=';')
+        csv_writer.writerow(cols)
+
+
 def update_stack(stack: np.array, layer: np.array):
 
     if stack is None:
         return layer
 
     return np.dstack((stack, layer))
+
+
+def write_csv(output_path: str, row: list) -> None:
+    """
+    Write a row in the CSV file.
+    :param output_path: path of the output file
+    :param row: row to write
+    :return: None
+    """
+    with open(f"{output_path}log_metrics.csv", 'a') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=';')
+        csv_writer.writerow(row)
