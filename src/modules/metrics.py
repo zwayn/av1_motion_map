9c55b52ba3fdef488bb463a65bff86694685774a
Julien Zouein <julien.zouein@gmail.com>
2023-12-19 10:28:04 +0100

    src/modules/metrics: add iqa and complexity metrics

--- src/modules/metrics.py
+++ src/modules/metrics.py
@@ -10,6 +10,7 @@ Python file used to define all the metrics used to evaluate the quality of the e
 """
 
 import numpy as np
+import tensorflow as tf
 from ..third_party.flowpy.flowpy import forward_warp
 
 
@@ -35,4 +36,37 @@ def interpolation_error(motion_vectors: np.ndarray, current_frame: np.ndarray, n
     interpolated_frame = forward_warp(current_frame, motion_vectors)
 
     return np.square(np.subtract(next_frame, interpolated_frame)).mean()
+
+
+def ms_ssim(original_frame: np.ndarray, encoded_frame: np.ndarray) -> float:
+    """
+    Compute the multi-scale structural similarity index between the current frame and the next frame.
+    :param original_frame: Original Frame
+    :param encoded_frame: Frame encoded in AV1
+    :return: ms-ssim
+    """
+    return tf.image.ssim_multiscale(original_frame, encoded_frame, max_val=255).numpy()
+
+
+def psnr(original_frame: np.ndarray, encoded_frame: np.ndarray) -> float:
+    """
+    Compute the peak signal-to-noise ratio between the current frame and the next frame.
+    :param original_frame: Original Frame
+    :param encoded_frame: Frame encoded in AV1
+    :return: psnr
+    """
+    return tf.image.psnr(original_frame, encoded_frame, max_val=255).numpy()
+
+
+def total_variation(frame: np.ndarray) -> float:
+    """
+    Compute the total variation of the motion vectors.
+
+    Total Variation can be used as a metric to compute the complexity of a frame. It measures the amount of variation in
+     pixel intensity, which can be an indicator of texture or edge complexity.
+
+    :param frame: motion vectors to evaluate
+    :return: total variation
+    """
+    return tf.image.total_variation(frame).numpy()
 
